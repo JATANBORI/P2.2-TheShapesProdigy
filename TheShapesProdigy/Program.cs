9eb985dfe5d56d330e32e1b730d7a729ca9e1a06
JATANBORI <j.atanbori@hull.ac.uk>
2022-03-02 20:46:39 +0000

    initial

---
+++ TheShapesProdigy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheShapesProdigy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Shapes Prodigy!");
+        }
+    }
+}
